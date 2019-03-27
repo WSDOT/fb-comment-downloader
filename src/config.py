02d9b36b0f7ba4229631d5d62b13ab0fe9e58607
Logan Sims <simsl@wsdot.wa.gov>
2019-03-27 10:41:57 -0700

    Update config.py
    Adds page access token env var

--- src/config.py
+++ src/config.py
@@ -1,9 +1,6 @@
 import os
 
-app_id = os.environ['APP_ID'] or "-1" # Facebook app ID. see https://developers.facebook.com/docs/apps/register/
-app_secret = os.environ['APP_SECRET'] or "-1" # DO NOT SHARE WITH ANYONE!
-
-access_token = app_id + "|" + app_secret
+access_token = os.environ['PAGE_ACCESS_TOKEN'] or "x"
 
 page_id = os.environ['PAGE_ID'] or "-1" # ID of the public Facebook page where comments will be downloaded from.
 page_name = os.environ['PAGE_NAME'] or "PAGE NAME" # Name of page, must be exactly as shown in page url.
